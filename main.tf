f3eb98d58fd2fa481ca2579f8f78317cd5454fd6
Evan de Guzman <evan.dg31@gmail.com>
2025-11-19 22:46:04 -0500

    deploying subnet and storage

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
 
 }
 
@@ -61,6 +62,7 @@ resource "azurerm_storage_account" "storage" {
     ip_rules                   = ["100.0.0.1"]
     virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
   }
+
   tags = local.tags
 }
 
